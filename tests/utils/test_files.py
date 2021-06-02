1b9cfdd64fb0bd3de714c6edb4424f08711acc67
Łukasz Piątkowski <piontec@gmail.com>
2021-06-02 13:25:16 +0200

    fix: add missing test resources

--- tests/utils/test_files.py
+++ tests/utils/test_files.py
@@ -3,7 +3,5 @@ from step_exec_lib.utils.files import get_file_sha256
 
 
 def test_get_sha256() -> None:
-    filename = os.path.join(
-        os.path.dirname(__file__), "../../../app-build-suite/tests", "build_steps", "res_test_helm", "Chart.yaml"
-    )
+    filename = os.path.join(os.path.dirname(__file__), "../../../step-exec-lib/tests", "resources", "Chart.yaml")
     assert get_file_sha256(filename) == "d7d772229b6187fd20906bf4c457b2077a630eacdaa193ac11482096e505c1bc"
