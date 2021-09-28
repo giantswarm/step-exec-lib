e89437820263b6971512bb538de1163ccae39a9b
Ross Fairbanks <ross@rossfairbanks.com>
2021-09-28 18:24:55 +0200

    Add #nosec and debug

--- step_exec_lib/utils/processes.py
+++ step_exec_lib/utils/processes.py
@@ -4,10 +4,16 @@ from typing import List, Any
 
 logger = logging.getLogger(__name__)
 
-_TOKEN_ARG = "--github-token"
+_GITHUB_TOKEN_ARG = "--github-token" # nosec
 
 def run_and_log(args: List[str], **kwargs: Any) -> subprocess.CompletedProcess:
+    logger.info("ARGS:")
+    logger.info(" ".join(args))
+
     sanitized_args = sanitize_args(args)
+
+    logger.info("SANITIZED:")
+    logger.info(" ".join(sanitized_args))
 
     logger.info("Running command:")
     logger.info(" ".join(sanitized_args))
@@ -27,8 +33,8 @@ def sanitize_args(args: List[str]) -> List[str]:
     res = []
 
     for arg in args:
-        if arg.startswith(_TOKEN_ARG):
-            res.append(f"{_TOKEN_ARG}=*****")
+        if arg.startswith(_GITHUB_TOKEN_ARG):
+            res.append(f"{_GITHUB_TOKEN_ARG}=*****")
         else:
             res.append(arg)
 
