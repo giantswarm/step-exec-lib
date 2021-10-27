04f141fa8e5987de9bbe1aa2fb9f99930e1c0157
Łukasz Piątkowski <piontec@gmail.com>
2021-10-27 17:48:01 +0200

    fix how run_end_handle_error_works

--- step_exec_lib/utils/processes.py
+++ step_exec_lib/utils/processes.py
@@ -1,6 +1,5 @@
 import logging
 import subprocess  # nosec: we need it to invoke binaries from system
-import sys  # nosec: stream stdout to get feedback on tests
 from typing import List, Any
 
 logger = logging.getLogger(__name__)
@@ -23,19 +22,12 @@ def run_and_handle_error(args: List[str], expected_error_text: str, **kwargs: An
     if "text" not in kwargs:
         kwargs["text"] = True
 
-    run_res = subprocess.run(args, **kwargs, check=False, stdout=sys.stdout, stderr=subprocess.STDOUT)  # nosec
+    run_res = subprocess.run(args, **kwargs, capture_output=True)  # nosec
     logger.info(run_res.stdout)
-
-    if run_res.returncode != 0:
-        try:
-            run_res.check_returncode()
-        except subprocess.CalledProcessError as e:
-            if expected_error_text in e.stdout:
-                logger.info(f"Found expected error text '{expected_error_text}', exit code: 0")
-                run_res.returncode = 0
-                return run_res
-            else:
-                logger.info(f"CalledProcessError: {e}, exit code: {e.returncode}")
-
     logger.info(f"Command executed, exit code: {run_res.returncode}.")
+
+    if run_res.returncode != 0 and expected_error_text and expected_error_text in run_res.stderr:
+        logger.info(f"Found expected error text '{expected_error_text}' in stderr, overriding exit code to 0")
+        run_res.returncode = 0
+
     return run_res
