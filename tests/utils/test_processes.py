3e4ee69e135440654d0faa70e4185b6c782ae040
Lukasz Piatkowski <lukasz@giantswarm.io>
2021-10-28 10:21:20 +0200

    fix how run_end_handle_error_works (#31)

---
+++ tests/utils/test_processes.py
@@ -0,0 +1,19 @@
+from typing import List
+
+import pytest
+
+from step_exec_lib.utils.processes import run_and_handle_error
+
+
+@pytest.mark.parametrize(
+    "args,expected_error,exit_code",
+    [
+        (["echo", "ok"], "", 0),
+        (["echo", "ok"], "blaaaaah", 0),
+        (["bash", "-c", "echo 'wrooong' && false"], "blaaaaah", 1),
+        (["bash", "-c", "echo 'wrooong' 1>&2 && false"], "ooo", 0),
+    ],
+)
+def test_run_and_handle_error(args: List[str], expected_error: str, exit_code: int) -> None:
+    res = run_and_handle_error(args, expected_error)
+    assert res.returncode == exit_code
